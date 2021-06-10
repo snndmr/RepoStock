604ad573bf41ab4bfbdf7b1ec08c4afc119533c4
SNN <snndmrprx@gmail.com>
2021-06-10 18:08:18 +0300

    profile ui designed

--- app/src/main/java/com/snn/repostock/FragmentProfile.kt
+++ app/src/main/java/com/snn/repostock/FragmentProfile.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class FragmentProfile : Fragment() {
     override fun onCreateView(
@@ -13,5 +17,70 @@ class FragmentProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val workHours = (1..14).map { (7..14).random() }
+        val numberOfCustomers = (1..14).map { (5..100).random() }
+        val numberOfSales = numberOfCustomers.map { item -> item + (1..20).random() }
+
+        aa_chart_view_daily_salary.aa_drawChartWithChartModel(
+            AAChartModel()
+                .chartType(AAChartType.Pie)
+                .title("Salary")
+                .yAxisTitle("")
+                .series(
+                    arrayOf(
+                        AASeriesElement()
+                            .name("Salary")
+                            .data(
+                                arrayOf(
+                                    arrayOf("Salary", 3336.2),
+                                    arrayOf("Promotion", 223.8),
+                                    arrayOf("Gift Card", 88.5)
+                                )
+                            )
+                    )
+                )
+        )
+
+        aa_chart_view_work_hours.aa_drawChartWithChartModel(
+            AAChartModel()
+                .chartType(AAChartType.Spline)
+                .title("Hours")
+                .yAxisTitle("")
+                .series(
+                    arrayOf(
+                        AASeriesElement()
+                            .name("Hours")
+                            .data(
+                                workHours.toTypedArray()
+                            )
+                    )
+                )
+        )
+
+        aa_chart_view_daily_sales.aa_drawChartWithChartModel(
+            AAChartModel()
+                .chartType(AAChartType.Column)
+                .title("Sales")
+                .yAxisTitle("")
+                .series(
+                    arrayOf(
+                        AASeriesElement()
+                            .name("Customers")
+                            .data(
+                                numberOfCustomers.toTypedArray()
+                            ),
+                        AASeriesElement()
+                            .name("Sales")
+                            .data(
+                                numberOfSales.toTypedArray()
+                            )
+                    )
+                )
+        )
     }
 }
--- app/src/main/java/com/snn/repostock/MainActivity.kt
+++ app/src/main/java/com/snn/repostock/MainActivity.kt
@@ -3,6 +3,7 @@ package com.snn.repostock
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet_persistent_profile.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
